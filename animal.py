ffb5260b9b4ccd69515d5e969c562af0b303c7ed
PringleElUno <pringlel@oregonstate.edu>
2024-09-26 13:32:29 -0700

    Assignment 1

---
+++ animal.py
@@ -0,0 +1,7 @@
+# Author: Luis Pringle
+# GitHub username: PringleElUno
+# Date: 09/25/2024
+# Description: Asks the user for their favorite animal and then prints out that that is their favorite animal
+
+fave_animal = input("Please enter your favorite animal: ")
+print("Your favorite animal is the " + fave_animal + ".")
